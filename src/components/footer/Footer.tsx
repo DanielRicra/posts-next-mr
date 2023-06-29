67a05c6749e62cb2e84ed0616e161ea2ec34e250
DanielRicra <daniel.int@outlook.com>
2023-06-29 17:50:49 -0500

    Update footer to be completed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image';
+
 const Footer = () => {
    return (
-      <div>
-         <div>©2023. All rights reserved</div>
-         <div></div>
+      <div className='flex h-[50px] items-center justify-between text-sm'>
+         <div className=''>©2023. All rights reserved</div>
+         <div className='flex items-center gap-[10px] [&>img]:opacity-80 [&>img]:cursor-pointer'>
+            <Image src='/1.png' alt='Facebook' width={20} height={20} />
+            <Image src='/2.png' alt='Instagram' width={20} height={20} />
+            <Image src='/3.png' alt='Twitter' width={20} height={20} />
+            <Image src='/4.png' alt='Youtube' width={20} height={20} />
+         </div>
       </div>
    );
 };
