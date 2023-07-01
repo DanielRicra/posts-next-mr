8650d1c845e536889868ffe74ff5127a2bf0be6e
DanielRicra <daniel.int@outlook.com>
2023-07-01 00:47:00 -0500

    Add portfolio dinamic post by category

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,45 +1,47 @@
 import Button from '@/components/button/Button';
 import Image from 'next/image';
 import React from 'react';
+import { items } from './data';
+import { notFound } from 'next/navigation';
+
+const getData = (category: string) => {
+   if (
+      category !== 'applications' &&
+      category !== 'illustrations' &&
+      category !== 'websites'
+   ) {
+      return notFound();
+   }
+
+   const data = items[category];
+   return data;
+};
 
 const Category = ({ params }: { params: { category: string } }) => {
+   const data = getData(params.category);
+
    return (
       <div className=''>
          <h1 className='capitalize text-primary'>{params.category}</h1>
-         <div className='flex mt-10 mb-20 gap-12 odd:flex-row-reverse'>
-            <div className='flex-1 flex flex-col gap-4 justify-center'>
-               <h2 className='text-4xl'>Test</h2>
-               <p className='text-lg'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-               </p>
-               <Button url='#' text='See More' />
+         {data.map((post) => (
+            <div className='flex mt-10 mb-20 gap-12 odd:flex-row-reverse' key={post.id}>
+               <div className='flex-1 flex flex-col gap-4 justify-center'>
+                  <h2 className='text-4xl'>{post.title}</h2>
+                  <p className='text-lg'>
+                     {post.desc}
+                  </p>
+                  <Button url='#' text='See More' />
+               </div>
+               <div className='flex-1 relative h-[440px]'>
+                  <Image
+                     fill={true}
+                     src={post.image}
+                     alt='radom'
+                     className='object-cover'
+                  />
+               </div>
             </div>
-            <div className='flex-1 relative h-[440px]'>
-               <Image
-                  fill={true}
-                  src='https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                  alt='radom'
-                  className='object-cover'
-               />
-            </div>
-         </div>
-         <div className='flex mt-10 mb-20 gap-12 odd:flex-row-reverse'>
-            <div className='flex-1 flex flex-col gap-4 justify-center'>
-               <h2 className='text-4xl'>Test</h2>
-               <p className=''>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-               </p>
-               <Button url='#' text='See More' />
-            </div>
-            <div className='flex-1 relative h-[440px]'>
-               <Image
-                  fill={true}
-                  src='https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                  alt='radom'
-                  className='object-cover'
-               />
-            </div>
-         </div>
+         ))}
       </div>
    );
 };
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -6,13 +6,13 @@ const Portfolio = () => {
          <h1 className='my-4'>Chose a gallery</h1>
          <div className='flex gap-12 [&>a]:bg-cover [&>a]:rounded-md [&>a]:relative [&>a]:border-4 [&>a]:border-[#bbb]'>
             <Link href='/portfolio/illustrations' className='w-[300px] h-[400px] bg-[url("/illustration.png")] group'>
-               <span className='absolute right-2 bottom-2 text-3xl group-hover:text-primary'>Illustrations</span>
+               <span className='absolute right-2 bottom-2 text-3xl group-hover:text-primary text-white'>Illustrations</span>
             </Link>
             <Link href='/portfolio/websites' className='w-[300px] h-[400px] bg-[url("/websites.jpg")] group'>
-               <span className='absolute right-2 bottom-2 text-3xl group-hover:text-primary'>Websites</span>
+               <span className='absolute right-2 bottom-2 text-3xl group-hover:text-primary text-white'>Websites</span>
             </Link>
             <Link href='/portfolio/applications' className='w-[300px] h-[400px] bg-[url("/apps.jpg")] group'>
-               <span className='absolute right-2 bottom-2 text-3xl group-hover:text-primary'>Applications</span>
+               <span className='absolute right-2 bottom-2 text-3xl group-hover:text-primary text-white'>Applications</span>
             </Link>
          </div>
       </div>
