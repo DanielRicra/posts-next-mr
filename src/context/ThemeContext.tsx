1ed0fc86bc8a11b2a6f058e06c27bd7997b69392
DanielRicra <daniel.int@outlook.com>
2023-07-01 00:48:51 -0500

    Update ThemeContext to remove setind dark clas to document

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -13,20 +13,12 @@ const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
    const [theme, setTheme] = useState<ThemeContextType['theme']>('light');
 
    const toggleTheme = () => {
-      if (theme === 'light') {
-         setTheme('dark');
-         document.documentElement.classList.add('dark')
-      } else {
-         setTheme('light');
-         document.documentElement.classList.remove('dark')
-      }
+      setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
    };
 
    return (
       <ThemeContext.Provider value={{ theme, toggleTheme }}>
-         <div className={theme === 'dark' ? 'dark' : ''}>
-            {children}
-         </div>
+         <div className={theme === 'dark' ? 'dark' : ''}>{children}</div>
       </ThemeContext.Provider>
    );
 };
