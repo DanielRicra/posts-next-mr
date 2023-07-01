e1b9d5f65861305581df56c47258b4d6993214b1
DanielRicra <daniel.int@outlook.com>
2023-07-01 00:28:49 -0500

    update layout.tsx
    Add AuthProvider tha is client component that wraps its children in a SessionProvider from next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import Navbar from '@/components/navbar/Navbar';
-import './globals.css';
 import { Inter } from 'next/font/google';
 import Footer from '@/components/footer/Footer';
+import Navbar from '@/components/navbar/Navbar';
 import ThemeProvider from '@/context/ThemeContext';
+import AuthProvider from '@/components/auth-provider/AuthProvider';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,11 +21,13 @@ export default function RootLayout({
       <html lang='en'>
          <body className={inter.className}>
             <ThemeProvider>
-               <div className='container dark:text-white dark:bg-[#091420] duration-500 ease-in transition-colors'>
-                  <Navbar />
-                  {children}
-                  <Footer />
-               </div>
+               <AuthProvider>
+                  <div className='container dark:text-white dark:bg-[#091420] duration-500 ease-in transition-colors'>
+                     <Navbar />
+                     {children}
+                     <Footer />
+                  </div>
+               </AuthProvider>
             </ThemeProvider>
          </body>
       </html>
