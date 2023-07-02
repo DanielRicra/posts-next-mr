0b09fd44c7576287ee5140e7d20dd741541a1a42
DanielRicra <daniel.int@outlook.com>
2023-07-01 20:53:04 -0500

    Update post/[id] route to add Delete endpoint

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,11 +1,10 @@
-import { NextApiRequest } from 'next';
 import { NextResponse } from 'next/server';
 import { IPost } from '@/types';
 import Post from '@/models/Post';
 import connect from '@/utils/db';
 
 export const GET = async (
-   _request: NextApiRequest,
+   _request: Request,
    { params }: { params: { id: string } }
 ) => {
    const { id } = params;
@@ -20,6 +19,27 @@ export const GET = async (
       }
 
       return NextResponse.json(post, { status: 200 });
+   } catch (error) {
+      return new NextResponse('Database error!', { status: 500 });
+   }
+};
+
+export const DELETE = async (
+   _request: Request,
+   { params }: { params: { id: string } }
+) => {
+   const { id } = params;
+
+   try {
+      await connect();
+
+      const post = await Post.findByIdAndDelete(id);
+
+      if (!post) {
+         return NextResponse.json('Post Not Found', { status: 404 });
+      }
+
+      return NextResponse.json('Post deleted successfully', { status: 200 });
    } catch (error) {
       return new NextResponse('Database error!', { status: 500 });
    }
