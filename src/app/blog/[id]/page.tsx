2f9da69fede58134522502e64c09f87723d7faf6
DanielRicra <daniel.int@outlook.com>
2023-07-01 20:55:23 -0500

    Update blog relatesd pages

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,18 +1,35 @@
+import { IPost } from '@/types';
 import Image from 'next/image';
 import React from 'react';
+import { notFound } from 'next/navigation';
 
-const BlogPost = () => {
+async function getData(id: number) {
+   const response = await fetch(`http://localhost:3000/api/posts/${id}`);
+
+   if (!response.ok) {
+      return notFound();
+   }
+
+   return response.json() as Promise<IPost>;
+}
+
+export async function generateMetadata({ params }: { params: { id: number } }) {
+   const post = await getData(params.id);
+   return {
+      title: post.title,
+      description: post.desc,
+   };
+}
+
+const BlogPost = async ({ params }: { params: { id: number } }) => {
+   const data = await getData(params.id);
+
    return (
       <div className=''>
          <div className='flex'>
             <div className='flex-1 flex flex-col justify-between'>
-               <h1 className='text-4xl'>Main title</h1>
-               <p className='text-lg'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-                  minus a deserunt obcaecati labore, iure soluta ullam
-                  architecto nam suscipit qui rem. Beatae, quisquam consectetur?
-                  Labore exercitationem reprehenderit modi rem!
-               </p>
+               <h1 className='text-4xl'>{data.title}</h1>
+               <p className='text-lg'>{data.desc}</p>
                <div className='flex items-center gap-2'>
                   <Image
                      src='https://images.pexels.com/photos/816608/pexels-photo-816608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
@@ -21,26 +38,20 @@ const BlogPost = () => {
                      height={40}
                      className='w-10 h-10 rounded-full'
                   />
-                  <span className='text-base'>Lana Del Rey</span>
+                  <span className='text-base'>{data.username}</span>
                </div>
             </div>
             <div className='flex-1 h-72 relative'>
                <Image
-                  src='https://images.pexels.com/photos/816608/pexels-photo-816608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                  src={data.image}
                   alt='post image'
                   fill={true}
                   className='object-cover'
                />
             </div>
          </div>
-         <div className='mt-11 text-xl font-light text-[#bbb] text-justify'>
-            <p className=''>
-               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
-               praesentium itaque, est assumenda odit reiciendis asperiores
-               harum. Laboriosam, ab dolor.
-               <br />
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea magnam impedit, a recusandae sequi ad doloremque aliquam nesciunt fugit sint veritatis odio soluta, voluptatibus cupiditate, deserunt non quasi provident laudantium odit eligendi. Magnam, pariatur mollitia! Laboriosam voluptas officia temporibus adipisci!
-            </p>
+         <div className='mt-11 text-xl font-light dark:text-gray-200 text-gray-800 text-justify'>
+            <p className=''>{data.content}</p>
          </div>
       </div>
    );
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,41 +1,48 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { IPost } from '@/types';
 
-const Blog = () => {
+async function getData() {
+   const response = await fetch('http://localhost:3000/api/posts', { cache: 'no-store' });
+
+   if (!response.ok) {
+      throw new Error('Failed t fetch posts');
+   }
+
+   return response.json() as Promise<IPost[]>;
+}
+
+export const metadata = {
+   title: 'Posts app | Blog',
+   description: 'Some description',
+};
+
+const Blog = async () => {
+   const data = await getData();
+
    return (
       <div className=''>
-         <Link href='/blog/testId' className='flex items-center gap-10  mb-10'>
-            <div className=''>
-               <Image
-                  alt='Galaxy'
-                  src='https://images.pexels.com/photos/816608/pexels-photo-816608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                  width={400}
-                  height={250}
-                  className='object-cover w-[400px] h-[250px]'
-               />
-            </div>
-            <div className=''>
-               <h1 className='mb-2'>Test 1</h1>
-               <p className='text-lg text-[#999]'>Description 1</p>
-            </div>
-         </Link>
-
-         <Link href='/blog/testId' className='flex items-center gap-10  mb-10'>
-            <div className=''>
-               <Image
-                  alt='Galaxy'
-                  src='https://images.pexels.com/photos/816608/pexels-photo-816608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                  width={400}
-                  height={250}
-                  className='object-cover w-[400px] h-[250px]'
-               />
-            </div>
-            <div className=''>
-               <h1 className='mb-2'>Test 1</h1>
-               <p className='text-lg text-[#999]'>Description 1</p>
-            </div>
-         </Link>
+         {data.map((post) => (
+            <Link
+               href={`/blog/${post._id}`}
+               className='flex items-center gap-10  mb-10'
+               key={post._id.toString()}
+            >
+               <div className='w-[400px] h-[250px] overflow-hidden relative'>
+                  <Image
+                     alt='Galaxy'
+                     src={post.image}
+                     fill
+                     className='object-cover'
+                  />
+               </div>
+               <div className='flex-1'>
+                  <h1 className='mb-2'>{post.title}</h1>
+                  <p className='text-lg text-[#999]'>{post.desc}</p>
+               </div>
+            </Link>
+         ))}
       </div>
    );
 };
