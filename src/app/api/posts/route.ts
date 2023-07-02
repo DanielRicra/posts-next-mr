89ef909f02fbbd8a6130274d6991088928480785
DanielRicra <daniel.int@outlook.com>
2023-07-01 20:54:21 -0500

    Update posts route to add POST endpoint
    Update GET endpoint to get a user posts or all if there is no user

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -3,13 +3,38 @@ import connect from '@/utils/db';
 import Post from '@/models/Post';
 import { IPost } from '@/types';
 
-export const GET = async () => {
+export const GET = async (request: Request) => {
+   const url = new URL(request.url);
+
+   const username = url.searchParams.get('username');
+   let posts: IPost[];
+
    try {
       await connect();
-
-      const posts: IPost[] = await Post.find();
+      if (username) {
+         posts = await Post.find({ username });
+      } else {
+         posts = await Post.find();
+      }
 
       return NextResponse.json(posts, { status: 200 });
+   } catch (error) {
+      return new NextResponse('Database error!', { status: 500 });
+   }
+};
+
+export const POST = async (request: Request) => {
+   const body = await request.json();
+
+   const newPost = new Post(body);
+
+
+   try {
+      await connect();
+      
+      await newPost.save();
+
+      return NextResponse.json('The post has been created', { status: 201 });
    } catch (error) {
       return new NextResponse('Database error!', { status: 500 });
    }
